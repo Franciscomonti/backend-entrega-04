bfea0c168f6e725e78f55012e542164159627c7d
Francsico Monti <montifranciscojavier@gmail.com>
2022-11-03 16:32:12 -0300

    agregado de route get por id

--- routes/routes_prod.js
+++ routes/routes_prod.js
@@ -9,6 +9,17 @@ router.get('/productos', (req, res) => {
     res.send(Producto.productos);
 });
 
+router.get('/productos/:id', (req, res) => {
+    let productoId = Number(req.params.id);
+    let productSelected = Producto.productos.find(item  => item.id === productoId)
+
+    if(productSelected){
+    res.send(productSelected)
+    }else{
+        res.status(404).send({error: 'Id de producto incorrecto'});
+    }
+})
+
 //toma los datos desde el html le asigana un id y los carga con el metodo construtcor y los muestra
 router.post('/productos', (req, res) => {
     let { titulo, precio, thumbnail } = req.body;
